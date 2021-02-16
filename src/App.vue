1224a1efd78bb20dadc4a072caaa6a1584f98292
Diyar <diyar.oktay@outlook.com>
2021-02-16 17:43:12 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-grid theme="compact" :source="rows" :columns="columns"></v-grid>
+</template>
+
+
+<script>
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import VGrid from "@revolist/vue3-datagrid";
+export default defineComponent({
+  name: "App",
+  data() {
+    return {
+      columns: [{ prop: "name" }, { prop: "details" }],
+      rows: [
+        {
+          name: "1",
+          details: "Item 1"
+        }
+      ]
+    };
+  },
+  components: {
+    HelloWorld,
+    VGrid
+  }
+});
+</script>
